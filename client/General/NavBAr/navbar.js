cc49bcda0d6c6490296d636109ca71dff95c4106
Brian McGonagill <bmcgonag@gmail.com>
2020-12-12 19:49:05 -0600

    Getting basics setup still

---
+++ client/General/NavBAr/navbar.js
@@ -0,0 +1,27 @@
+Template.navbar.onCreated(function() {
+
+});
+
+Template.navbar.onRendered(function() {
+
+});
+
+Template.navbar.helpers({
+
+});
+
+Template.navbar.events({
+    "click .navItem" (event) {
+        let navPage = event.currentTarget.id;
+        
+        if (navPage == "hometab") {
+            FlowRouter.go('/');
+        } else if (navPage == "setuptab") {
+            FlowRouter.go('/setup');
+        } else if (navPage == "accounttab") {
+            FlowRouter.go('/myaccount');
+        } else if (navPage == "dashboardtab") {
+            FlowRouter.go('/dashboard');
+        }
+    },
+});
