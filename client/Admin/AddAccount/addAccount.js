002f66941529446548bd35ae3ece0acb6306dfd4
Brian McGonagill <bmcgonag@gmail.com>
2020-12-20 07:33:15 -0600

    more work

--- client/Admin/AddAccount/addAccount.js
+++ client/Admin/AddAccount/addAccount.js
@@ -6,7 +6,7 @@ Template.addAccount.onCreated(function() {
     this.subscribe("acctTypes");
     this.subscribe("transTypes");
     this.subscribe("userList");
-    this.subscribe('allAccounts');
+    this.subscribe("allAccounts");
 });
 
 Template.addAccount.onRendered(function() {
@@ -17,6 +17,9 @@ Template.addAccount.helpers({
     accountFor: function() {
         let userId = Session.get("addAccountForUserId");
         return Meteor.users.find({ _id: userId });
+    },
+    currentAccounts: function() {
+        return MyAccount.find({});
     },
 });
 
